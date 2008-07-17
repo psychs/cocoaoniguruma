d9c6fa02715235971166dc9c1866c2926124c577
Satoshi Nakagawa <psychs@limechat.net>
2008-07-18 06:39:56 +0900

    removed unnecessary encodings.

--- samples/core-sample/CocoaOniguruma/oniguruma/regext.c
+++ samples/core-sample/CocoaOniguruma/oniguruma/regext.c
@@ -97,62 +97,13 @@ conv_encoding(OnigEncoding from, OnigEncoding to, const UChar* s, const UChar* e
 {
   int len = end - s;
 
-  if (to == ONIG_ENCODING_UTF16_BE) {
-    if (from == ONIG_ENCODING_ASCII) {
-      *conv = (UChar* )xmalloc(len * 2);
-      CHECK_NULL_RETURN_MEMERR(*conv);
-      *conv_end = *conv + (len * 2);
-      conv_ext0be(s, end, *conv);
-      return 0;
-    }
-    else if (from == ONIG_ENCODING_UTF16_LE) {
-    swap16:
-      *conv = (UChar* )xmalloc(len);
-      CHECK_NULL_RETURN_MEMERR(*conv);
-      *conv_end = *conv + len;
-      conv_swap2bytes(s, end, *conv);
-      return 0;
-    }
-  }
-  else if (to == ONIG_ENCODING_UTF16_LE) {
+  if (to == ONIG_ENCODING_UTF16_LE) {
     if (from == ONIG_ENCODING_ASCII) {
       *conv = (UChar* )xmalloc(len * 2);
       CHECK_NULL_RETURN_MEMERR(*conv);
       *conv_end = *conv + (len * 2);
       conv_ext0le(s, end, *conv);
       return 0;
-    }
-    else if (from == ONIG_ENCODING_UTF16_BE) {
-      goto swap16;
-    }
-  }
-  if (to == ONIG_ENCODING_UTF32_BE) {
-    if (from == ONIG_ENCODING_ASCII) {
-      *conv = (UChar* )xmalloc(len * 4);
-      CHECK_NULL_RETURN_MEMERR(*conv);
-      *conv_end = *conv + (len * 4);
-      conv_ext0be32(s, end, *conv);
-      return 0;
-    }
-    else if (from == ONIG_ENCODING_UTF32_LE) {
-    swap32:
-      *conv = (UChar* )xmalloc(len);
-      CHECK_NULL_RETURN_MEMERR(*conv);
-      *conv_end = *conv + len;
-      conv_swap4bytes(s, end, *conv);
-      return 0;
-    }
-  }
-  else if (to == ONIG_ENCODING_UTF32_LE) {
-    if (from == ONIG_ENCODING_ASCII) {
-      *conv = (UChar* )xmalloc(len * 4);
-      CHECK_NULL_RETURN_MEMERR(*conv);
-      *conv_end = *conv + (len * 4);
-      conv_ext0le32(s, end, *conv);
-      return 0;
-    }
-    else if (from == ONIG_ENCODING_UTF32_BE) {
-      goto swap32;
     }
   }
 
